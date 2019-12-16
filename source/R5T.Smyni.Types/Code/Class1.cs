0eee9c6d6fdbf5f713f05fe3f17fb847e1a8cd8e
MinexAutomation <minex.automation@gmail.com>
2019-12-16 14:42:41 -0800

    Added file-extensions.

--- source/R5T.Smyni.Types/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Smyni.Types
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Smyni.Types/Code/Classes/FileExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Smyni.Types
+{
+    public static class FileExtensions
+    {
+        public static readonly string SolutionFileExtension = "sln";
+    }
+}
